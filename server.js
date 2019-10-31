cb5edf76a0d7f88c245394747f5fc57c2617ae47
mogwai <feelirabs@gmail.com>
2019-10-31 02:38:11 +0100

    Refactored server.js and imported as router to index.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = 'express';
+const express = require('express');
 
-const server = express();
+const router = express.Router();
 
-server.get('/', (req, res) => {
+router.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
+//Logger: console.logs request method, url and time of operation.
 function logger(req, res, next) {
+  console.log(
+    `This has been a ${req.method} request to ${req.url}. This operation happened at ${new Date()}`
+  );
 
-};
+  next();
+}
 
-module.exports = server;
+
+
+module.exports = router;
